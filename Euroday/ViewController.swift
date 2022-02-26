dd44ccbdb56eb0e8de0828be6cb62b80598ce84f
Nikita <tardpipl@gmail.com>
2022-02-26 16:52:46 +0200

    Added camera to ViewController and custom white button

--- Euroday/ViewController.swift
+++ Euroday/ViewController.swift
@@ -5,29 +5,108 @@
 //  Created by Ņikita Roldugins on 26/02/2022.
 //
 
+import AVFoundation
 import UIKit
+
 
 class ViewController: UIViewController {
     
-    private let button: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y:0, width: 200, height: 50))
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.setTitle("Tap me", for: .normal)
-        button.layer.cornerRadius = 15
+    var session: AVCaptureSession?
+    let output = AVCapturePhotoOutput()
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    private let shutterButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 75, height: 75))
+        button.layer.cornerRadius = 37.5
+        button.layer.borderWidth = 6
+        button.layer.borderColor = UIColor.white.cgColor
         return button
     }()
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(button)
-        button.center = view.center
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        view.addSubview(shutterButton)
+        checkCameraPermission()
+        
+        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
     }
     
-    @objc private func didTapButton() {
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+        
+        shutterButton.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height - 130)
+    }
+    
+    
+    private func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else { return }
+                DispatchQueue.main.async { self?.setUpCamera() }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setUpCamera()
+        @unknown default:
+            break
+        }
+    }
+    
+    
+    private func setUpCamera() {
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                
+                
+                session.startRunning()
+                self.session = session
+            }
+            catch { print(error) }
+        }
+    }
+    
+    
+    @objc private func didTapTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+        
         let vc = DetailViewController()
-        present(vc, animated: true)
+        DispatchQueue.main.async { self.present(vc, animated: true) }
     }
 }
 
+
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else {
+            return
+        }
+        let image = UIImage(data: data)
+        
+        session?.stopRunning()
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.frame = view.bounds
+        view.addSubview(imageView)
+    }
+}
